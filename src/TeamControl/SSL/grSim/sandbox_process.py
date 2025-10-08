4600381ec7f652f98c8a812ff7d5becd9c2bb3b8
lisagraf <lgraf@informatik.uni-freiburg.de>
2025-10-08 12:56:11 +0200

    Remove goalie and Voronoi planner

--- src/TeamControl/SSL/grSim/sandbox_process.py
+++ src/TeamControl/SSL/grSim/sandbox_process.py
@@ -4,7 +4,7 @@ from TeamControl.robot.Movement import RobotMovement
 
 def sandbox_process(wm):
     sim_ip = "127.0.0.1"
-    cmd_listen_port = 20010
+    cmd_listen_port = 20011
     is_yellow = True
     robot_id = 1
     sender = grSimSender(ip=sim_ip,port=cmd_listen_port,is_yellow=is_yellow)
@@ -12,6 +12,11 @@ def sandbox_process(wm):
     version = 0 # vision version
     robot_pos = 0,0,0
     ball = 0,0
+
+    #cmd = RobotCommand(robot_id=1, vx=500, vy=0, w=0, kick=0, dribble=0)
+    #sender.send_command(cmd)
+    #print("Test command sent")
+
     while True:
         # This is running
         # get_update
@@ -27,7 +32,7 @@ def sandbox_process(wm):
                 robot_pos = 0,0,0
                 ball = 0,0
                 
-        vx, vy, w= RobotMovement.velocity_to_target(robot_pos=robot_pos, target=ball)
+        vx, vy, w = RobotMovement.velocity_to_target(robot_pos=robot_pos, target=ball)
         
-        cmd = RobotCommand(1, vx, vy, w, 0, 0)
+        cmd = RobotCommand(robot_id=1, vx=vx, vy=vy, w=w, kick=0, dribble=0)
         sender.send_command(cmd)
