e3d2336e12e6e8f5cb525afa967f1e107e5a74ee
Emma@Jetson <20544166@student.westernsydney.edu.au>
2026-01-16 18:54:42 +1100

    changed to get robot position with local robot id provided

--- src/TeamControl/behaviour_tree/common_trees.py
+++ src/TeamControl/behaviour_tree/common_trees.py
@@ -32,10 +32,10 @@ class GetWorldPositionUpdate(py_trees.behaviour.Behaviour):
                 if self.frame.ball is not None:
                     self.ball_last_known = self.frame.ball.position
                     self.bb.ball_pos = self.ball_last_known
-                    print(self.ball_last_known)
-                    our_robots = self.frame.get_yellow_robots(isYellow=self.isYellow)
-                    self.bb.our_robots = our_robots
-                    
+                    # print(self.ball_last_known)
+                our_robots = self.frame.get_yellow_robots(isYellow=self.isYellow)
+                self.bb.our_robots = our_robots
+                
             return py_trees.common.Status.SUCCESS
         # otherwise keep running
         return py_trees.common.Status.RUNNING
@@ -102,28 +102,30 @@ class SendRobotCommand(py_trees.behaviour.Behaviour):
         
 
 class GetRobotIDPosition(py_trees.behaviour.Behaviour):
-    def __init__(self,robot_id:int=0):
-        self.robot_id = robot_id
+    def __init__(self):
         name = "GetRobotIDPosition"
         super().__init__(name)
-        
+        self.bb = py_trees.blackboard.Client(name="GetRobotIDPosition")
+        self.bb.register_key(key="robot_id", access=py_trees.common.Access.READ)
+        self.bb.register_key(key="our_robots", access=py_trees.common.Access.READ)
+        self.bb.register_key(key="robot_pos", access=py_trees.common.Access.WRITE)
+        self.robot_id = self.bb.robot_id
         
     def setup(self,logger=None):
         if logger is not None:
             self.logger = logger
-        self.bb = py_trees.blackboard.Client(name="GetRobotIDPosition")
-        # self.bb.register_key(key="robot_id", access=py_trees.common.Access.READ)
-        self.bb.register_key(key="our_robots", access=py_trees.common.Access.READ)
-        self.bb.register_key(key="robot_pos", access=py_trees.common.Access.WRITE)
+        
         pass
     
     def update(self) -> py_trees.common.Status:
         # gets the robot position base on robot_id
 
         our_robots = self.bb.our_robots
-        if our_robots is not None and isinstance(our_robots[self.robot_id],object):
+        if our_robots is not None:
             robot = our_robots[self.robot_id]
             # store position
+            if isinstance(robot,int):
+                return py_trees.common.Status.FAILURE
             self.bb.robot_pos = robot.position
             self.logger.info(f"[GetRobotIDPosition] Robot {self.robot_id} position: {robot.position}")
             return py_trees.common.Status.SUCCESS
